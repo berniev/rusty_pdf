35bfde18efcd77cd1e65eba7af6f6f3b607a024b
berniev <618593+berniev@users.noreply.github.com>
2026-05-06 13:21:50 +1000

    Refactor: streamline `Pdf` struct initialization and update `finalize` to use existing fields

--- src/pdf.rs
+++ src/pdf.rs
@@ -14,19 +14,28 @@ pub struct Pdf {
     pub object_ops: Rc<RefCell<ObjectOps>>,
     pub page_ops: PageOps,
     pub graphics_ops: GraphicsOps,
+    pub catalog_ops: CatalogOps,
+    pub xref_ops: XRefOps,
+    pub trailer: Trailer,
 }
 
 impl Pdf {
     pub fn new() -> Result<Self, PdfError> {
         let object_ops = Rc::new(RefCell::new(ObjectOps::new()));
-        let page_ops = PageOps::new(Rc::clone(&object_ops))?;
+        let mut page_ops = PageOps::new(Rc::clone(&object_ops))?;
         let graphics_ops = GraphicsOps::new(Rc::clone(&object_ops));
+        let catalog_object_number = object_ops.borrow_mut().next_object_number();
+        let catalog_ops = CatalogOps::new(catalog_object_number.clone(), &mut page_ops)?;
+        let trailer = Trailer::new(object_ops.borrow_mut().next_object_number())?;
 
         let pdf = Pdf {
             version: Version::default(),
             object_ops,
             page_ops,
             graphics_ops,
+            catalog_ops,
+            xref_ops:XRefOps::new(),
+            trailer,
         };
 
         Ok(pdf)
@@ -46,22 +55,19 @@ impl Pdf {
 
     pub fn finalize(&mut self, path: &str) -> Result<(), PdfError> {
         let mut build = || {
-            let catalog_object_number = self.object_ops.borrow_mut().next_object_number();
-            let mut catalog_ops =
-                CatalogOps::new(catalog_object_number.clone(), &mut self.page_ops)?;
-            let trailer = Trailer::new(
-                self.object_ops.borrow().last_object_number(),
-                catalog_object_number,
-            )?;
+            self.trailer
+                .set_last_object_number(self.object_ops.borrow().last_object_number())?;
             let mut xref_ops = XRefOps::new();
             let mut file = File::create(path)?;
 
             header::serialize(self.version, &mut file)?;
-            catalog_ops.serialize(self.version, &mut xref_ops, &mut file)?;
+            self.catalog_ops
+                .serialize(self.version, &mut xref_ops, &mut file)?;
             self.page_ops
                 .serialize(self.version, &mut xref_ops, &mut file)?;
             xref_ops.serialize(&mut file)?;
-            trailer.serialize(self.version, &mut xref_ops, &mut file)?;
+            self.trailer
+                .serialize(self.version, &mut xref_ops, &mut file)?;
 
             Ok(())
         };
